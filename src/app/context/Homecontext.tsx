533bb28b694fe58e591c82109c061041ba5b6d0b
PedroHercules0810 <angela..souza@alunos.ufersa.edu.br>
2024-09-02 08:00:41 -0300

    Num lembro

---
+++ src/app/context/Homecontext.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { createContext, ReactNode, useState } from "react";
+
+type HomeContextData = {
+    contador: number;
+    incremento: () => void;
+}
+
+export const HomeContext = createContext({} as HomeContextData);
+
+type ProviderProps = {
+    children : ReactNode;
+}
+
+const HomeContextProvider = ({children}:ProviderProps) => {
+    const [contador, setContador] = useState(0);
+
+    const incremento = () =>{
+        setContador(contador + 1);
+    }
+    return(
+        <HomeContext.Provider value={
+            {
+                contador,
+                incremento
+            }
+        }>
+            {children}
+        </HomeContext.Provider>
+    )
+} 
+
+export default HomeContextProvider;
