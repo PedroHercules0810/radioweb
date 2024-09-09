5acfa1b60d51876d5b5e81c5a38ef2f192444629
PedroHercules0810 <pedrosouzagrangeiro@gmail.com>
2024-09-09 16:40:29 -0300

    Tocando musga

---
+++ src/app/dados/music.ts
@@ -0,0 +1,31 @@
+type Music = {
+    nome: string;
+    author: string;
+    description: string;
+    urlAudio: string;
+    image: string;
+}
+
+export const musics: Music[] = [
+    {
+        nome: "Maitake",
+        author: "Vtuber",
+        description: "GURUGURU GURUGURU",
+        urlAudio: "audios/Maitake.mp3",
+        image: "Imagem da Música"
+    },
+    {
+        nome: "Fear of the Dark",
+        author: "Iron Maiden",
+        description: "FEAR OF THE DARK",
+        urlAudio: "audios/Fear of the Dark .mp3",
+        image: "Imagem da Música"
+    },
+    {
+        nome: "Numb",
+        author: "Linkin Park",
+        description: "I",
+        urlAudio: "audios/Numb.mp3",
+        image: "Imagem da Música"
+    }
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,52 @@
 'use client'
-import { useContext } from "react";
-//import { Fa6, FaCirclePlus } from "react-icons/fa6";
-import { IoPlayCircle, IoPauseCircle } from "react-icons/io5";
+
+import Image from "next/image";
 import { HomeContext } from "./context/Homecontext";
+import { useContext } from "react";
+import { GrChapterNext, GrChapterPrevious } from "react-icons/gr";
+import { ImPlay3, ImPause2 } from "react-icons/im";
+import { musics } from "./dados/music";
 
 export default function Home() {
+  const context = useContext(HomeContext);
+
+  if (!context) {
+    return <div>Carregando</div>;
+  }
+
   const {
-    contador,
-    incremento,
-    playing,
-    playing_texto,
-    togglePlaying
-  } = useContext(HomeContext);
+    playing, configPlayPause,
+    nomeMusica, passarMusica, voltarMusica,
+    selecionarMusica
 
-
+  } = context;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-[300px]">{playing_texto}</h1>
-      <button onClick={togglePlaying}>
-        {playing ? <IoPauseCircle className="text-[400px] text-[red]" /> : <IoPlayCircle className="text-[400px] text-[red]" />}
-      </button>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 italic">
+      <div className="flex flex-col items-center justify-center text-center italic">
+        <h1 className="text-[60px] italic font-sans">{nomeMusica}</h1>
+        <div className="flex items-center italic ">
+          <button onClick={() => voltarMusica()}>
+            <GrChapterPrevious  className="text-[80px]"/>
+          </button>
+          <button onClick={() => configPlayPause()}>
+            {playing ? <ImPause2 className="text-[80px]"/> : <ImPlay3 className="text-[80px]"/>}
+          </button>
+          <button onClick={() => passarMusica()}>
+            <GrChapterNext className="text-[80px] "/>
+          </button>
+        </div>
+        <div className="flex flex-col items-center italic">
+          {musics.map((music, i) => (
+            <button onClick={() => selecionarMusica(i)}>
+              <div key={i} className="hover:bg-red-700">
+                <p className="text-[30px] italic font-sans">{music.nome}</p>
+                <p className="text-[10px] italic font-sans">{music.author}</p>
+              </div>
+            </button>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
